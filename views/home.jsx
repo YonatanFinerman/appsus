2b676845240ad3cd25bfae517571dc32e3fb8d07
YonatanFinerman <julianmode808@gmail.com>
2022-12-28 10:13:09 +0200

    home page links

--- views/home.jsx
+++ views/home.jsx
@@ -1,7 +1,13 @@
+const { useParams, useNavigate, Link, Outlet } = ReactRouterDOM
+const { useEffect, useState } = React
 
 export function Home() {
-
+        const navigate = useNavigate()
     return <section className="home">
         <h1>Welcome to home page!</h1>
+        <h2 onClick={()=>{navigate("/mail")}}>to mail</h2>
+        <img src="" onClick={()=>{navigate("/mail")}} alt="" />
+        <img src=""  alt="" />
+        <img src="" onClick={()=>{navigate("/note")}} alt="" />
     </section>
 }
