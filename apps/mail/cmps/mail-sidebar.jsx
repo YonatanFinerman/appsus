0ea3418773d1d4457e3dda3f17d61b898df4be9f
YonatanFinerman <julianmode808@gmail.com>
2022-12-29 17:31:48 +0200

    made the side bat pretier

--- apps/mail/cmps/mail-sidebar.jsx
+++ apps/mail/cmps/mail-sidebar.jsx
@@ -3,11 +3,9 @@ export function MailSideBar(){
     return <section className="mail-sidebar">
        <button className='fa-solid pen compose-btn '>    Compose</button>
        <nav className="mail-nav"> 
-        {/* <button className='fa-solid pen '>  Inbox</button> */}
-        <button className="fa-regular star-nav">    Stared</button>
-        <button className="fa-regular box-nav">    Inbox</button>
-      
-        <button>Sent</button>
+        <button className="fa-regular star-nav" title="Starred">    Starred</button>
+        <button className="fa-regular inbox-nav" title="Inbox">    Inbox</button>
+        <button className="fa-regular sent-nav" title="Sent">   Sent</button>
         
        </nav>
     </section>
