0704362ab67ebd0305aefb485ce04c91808482f8
YonatanFinerman <julianmode808@gmail.com>
2022-12-31 10:11:33 +0200

    fixing bugs

--- views/home.jsx
+++ views/home.jsx
@@ -12,9 +12,9 @@ export function Home() {
         {/* <img src="/assets/img/Gmail-logo.png" className="mail-logo" onClick={()=>{navigate("/mail")}} alt="" />
         <img src="/assets/img/notes.png" onClick={()=>{navigate("/note")}} alt="" />
         <img src="/assets/img/books-logo1.svg"  alt="" /> */}
-        <img src="/assets/img/Gmail-logo.png" className="mail-logo" onClick={()=>{navigate("/mail")}} alt="" />
-        <img src="/assets/img/notes.png" onClick={()=>{navigate("/note")}} alt="" />
-        <img src="/assets/img/books-logo1.svg"  alt="" />
+        <img src="./assets/img/Gmail-logo.png" className="mail-logo" onClick={()=>{navigate("/mail")}} alt="" />
+        <img src="./assets/img/notes.png" onClick={()=>{navigate("/note")}} alt="" />
+        <img src="./assets/img/books-logo1.svg"  alt="" />
        
         </div>
     </section>
